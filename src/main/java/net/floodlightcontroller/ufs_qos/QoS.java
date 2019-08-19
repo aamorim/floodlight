f9baf7aa5652510b3a27e129272c9bda2fba44d2
alex <alex@VM-UFS-001>
2019-08-19 15:02:16 -0300

    Commit inicial

---
+++ src/main/java/net/floodlightcontroller/ufs_qos/QoS.java
@@ -0,0 +1,127 @@
+package net.floodlightcontroller.ufs_qos;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.projectfloodlight.openflow.protocol.OFFactory;
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFPacketQueue;
+import org.projectfloodlight.openflow.protocol.OFQueueGetConfigReply;
+import org.projectfloodlight.openflow.protocol.OFQueueGetConfigRequest;
+import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.protocol.queueprop.OFQueueProp;
+import org.projectfloodlight.openflow.protocol.queueprop.OFQueuePropMaxRate;
+import org.projectfloodlight.openflow.protocol.queueprop.OFQueuePropMinRate;
+import org.projectfloodlight.openflow.protocol.ver13.OFFactoryVer13;
+import org.projectfloodlight.openflow.protocol.ver13.OFQueuePropertiesSerializerVer13;
+import org.projectfloodlight.openflow.types.DatapathId;
+import org.projectfloodlight.openflow.types.OFPort;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.internal.IOFSwitchService;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.TimeUnit;
+import java.util.Set;
+import net.floodlightcontroller.packet.Ethernet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+
+public class QoS implements IOFMessageListener, IFloodlightModule {
+
+	
+	protected IFloodlightProviderService floodlightProvider;
+	protected Set<Long> macAddresses;
+	protected static Logger logger;
+	protected IOFSwitchService switchService;
+	
+	
+	@Override
+	public String getName() {
+		return QoS.class.getSimpleName();
+	}
+
+	@Override
+	public boolean isCallbackOrderingPrereq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCallbackOrderingPostreq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+		Collection<Class<? extends IFloodlightService>> collection = new ArrayList<Class<? extends IFloodlightService>>();
+		collection.add(IFloodlightProviderService.class);
+		return collection;
+	}
+
+	@Override
+	public void init(FloodlightModuleContext context) throws FloodlightModuleException {
+		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+	    macAddresses = new ConcurrentSkipListSet<Long>();
+	    logger = LoggerFactory.getLogger(QoS.class);
+	    switchService = context.getServiceImpl(IOFSwitchService.class);
+	}
+
+	@Override
+	public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
+		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+		
+	}
+
+	@Override
+	public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+		
+		
+		
+		Ethernet eth =
+                IFloodlightProviderService.bcStore.get(cntx,
+                                            IFloodlightProviderService.CONTEXT_PI_PAYLOAD); 
+        Long sourceMACHash = eth.getSourceMACAddress().getLong();
+        if (!macAddresses.contains(sourceMACHash)) {
+            macAddresses.add(sourceMACHash);
+            logger.info("MAC Address: {} seen on switch: {}",
+                    eth.getSourceMACAddress().toString(),
+                    sw.getId().toString());
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        return Command.CONTINUE;
+	}
+
+}
