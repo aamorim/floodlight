8275a53f7480e114abd529ffa7fb8a1f0204a0a7
alex <alex@VM-UFS-001>
2019-08-30 15:20:49 -0300

    dia 30/08/2019

--- src/main/java/net/floodlightcontroller/ufs_qos/QoS.java
+++ src/main/java/net/floodlightcontroller/ufs_qos/QoS.java
@@ -7,29 +7,45 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.projectfloodlight.openflow.protocol.OFActionType;
 import org.projectfloodlight.openflow.protocol.OFFactories;
 import org.projectfloodlight.openflow.protocol.OFFactory;
+import org.projectfloodlight.openflow.protocol.OFFlowAdd;
+import org.projectfloodlight.openflow.protocol.OFFlowMod;
 import org.projectfloodlight.openflow.protocol.OFMatchV3;
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFPacketIn;
+import org.projectfloodlight.openflow.protocol.OFPacketOut;
 import org.projectfloodlight.openflow.protocol.OFPacketQueue;
 import org.projectfloodlight.openflow.protocol.OFQueueGetConfigReply;
 import org.projectfloodlight.openflow.protocol.OFQueueGetConfigRequest;
 import org.projectfloodlight.openflow.protocol.OFType;
 import org.projectfloodlight.openflow.protocol.OFVersion;
+import org.projectfloodlight.openflow.protocol.action.OFAction;
+import org.projectfloodlight.openflow.protocol.action.OFActionEnqueue;
 import org.projectfloodlight.openflow.protocol.match.Match;
+import org.projectfloodlight.openflow.protocol.match.MatchField;
 import org.projectfloodlight.openflow.protocol.queueprop.OFQueueProp;
 import org.projectfloodlight.openflow.protocol.queueprop.OFQueuePropMaxRate;
 import org.projectfloodlight.openflow.protocol.queueprop.OFQueuePropMinRate;
 import org.projectfloodlight.openflow.protocol.ver13.OFFactoryVer13;
 import org.projectfloodlight.openflow.protocol.ver13.OFQueuePropertiesSerializerVer13;
 import org.projectfloodlight.openflow.types.DatapathId;
+import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.IPv4Address;
+import org.projectfloodlight.openflow.types.IpDscp;
+import org.projectfloodlight.openflow.types.IpProtocol;
+import org.projectfloodlight.openflow.types.MacAddress;
 import org.projectfloodlight.openflow.types.OFPort;
+import org.projectfloodlight.openflow.types.OFPortBitMap.Builder;
+import org.projectfloodlight.openflow.types.OFVlanVidMatch;
+import org.projectfloodlight.openflow.types.TransportPort;
 
 import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IOFMessageListener;
 import net.floodlightcontroller.core.IOFSwitch;
 import net.floodlightcontroller.core.internal.IOFSwitchService;
+import net.floodlightcontroller.core.internal.OFSwitchManager;
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
@@ -38,10 +54,14 @@ import net.floodlightcontroller.core.IFloodlightProviderService;
 import java.util.ArrayList;
 import java.util.Set;
 import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.EthernetTest;
+import net.floodlightcontroller.packet.IPv4;
+import net.floodlightcontroller.packet.IPv6;
 import net.floodlightcontroller.staticentry.IStaticEntryPusherService;
 import net.floodlightcontroller.storage.IResultSet;
 import net.floodlightcontroller.storage.IStorageSourceService;
 import net.floodlightcontroller.storage.StorageException;
+import net.floodlightcontroller.topology.Archipelago;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -58,6 +78,13 @@ public class QoS implements IOFMessageListener, IFloodlightModule, IQoSService {
 	protected IStaticEntryPusherService flowPusher;
 	protected List<QoSPolicy> policies; //Synchronized 
 	protected List<QoSTypeOfService> services; //Synchronized
+	protected OFFactory myFactory;
+	
+	//regex for dpid string, this can/needs to be more elegant. Maybe use of a Matcher
+	protected String dpidPattern = "^[\\d|\\D][\\d|\\D]:[\\d|\\D][\\d|\\D]:" +
+										"[\\d|\\D][\\d|\\D]:[\\d|\\D][\\d|\\D]:" +
+										"[\\d|\\D][\\d|\\D]:[\\d|\\D][\\d|\\D]:" +
+										"[\\d|\\D][\\d|\\D]:[\\d|\\D][\\d|\\D]$";
 	
 	
 	public static final String TABLE_NAME = "controller_qos";
@@ -123,7 +150,7 @@ public class QoS implements IOFMessageListener, IFloodlightModule, IQoSService {
 	}
  
 	/**
-	 * Esse método carrega todos os modulos a qual esse modulo depende.
+	 * Carrega todas as dependencias desse modulo
 	 */
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
@@ -140,10 +167,12 @@ public class QoS implements IOFMessageListener, IFloodlightModule, IQoSService {
 	public void init(FloodlightModuleContext context) throws FloodlightModuleException {
 
 		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+		switchService = context.getServiceImpl(IOFSwitchService.class);
 		logger = LoggerFactory.getLogger(QoS.class);
 		storageSource = context.getServiceImpl(IStorageSourceService.class);
 		policies = new ArrayList<QoSPolicy>();
 		services = new ArrayList<QoSTypeOfService>();
+		myFactory = OFFactories.getFactory(OFVersion.OF_13);
 
 	}
 
@@ -192,66 +221,57 @@ public class QoS implements IOFMessageListener, IFloodlightModule, IQoSService {
 		
         StringBuffer sb =  new StringBuffer("");
         Ethernet eth;
+        
+        
 
 		switch (msg.getType()) {
 		case PACKET_IN:
-			OFPacketIn pktIn = ( OFPacketIn ) msg;
-			sb.append("\ntype: " + pktIn.toString());
-			sb.append("\npacket_in [ ");
-            sb.append(sw.getId().toString());
-            sb.append(" -> Controller");
-            sb.append(" ]");
-
-            sb.append("\ntotal length: ");
-            sb.append(pktIn.getTotalLen());
-            sb.append("\nin_port: ");
-            sb.append(pktIn.getInPort());
-            sb.append("\ndata_length: ");
-            sb.append(pktIn.getTotalLen() - 0);
-            sb.append("\nbuffer: ");
-            sb.append(pktIn.getBufferId());
-
-            // If the conext is not set by floodlight, then ignore.
-            if (cntx != null) {
-            // packet type  icmp, arp, etc.
-                eth = IFloodlightProviderService.bcStore.get(cntx,
-                        IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-                if (eth != null)
-                	   sb.append("\ndata eth: ");
-                       sb.append(eth.toString());
-
-            }
-            logger.info("QoS-UFS data: {} ",sb.toString());
-            
-            //logger.debug("PACKET_IN recieved");
-        	byte[] packetData = OFMessage.getData(sw, msg, cntx);
-        	
-        	//Temporary match from packet to compare
-        	OFFactory my13Factory = OFFactories.getFactory(OFVersion.OF_13);
-        	Match myMatch = my13Factory.buildMatch().build();
-        	myMatch.loadFromPacket(packetData, OFPort.ZERO.getValue());
-        	checkIfQoSApplied(tmpMatch);
-        	
+			
+			if(cntx == null) {
+				return Command.CONTINUE;
+			}
+			
+			eth = IFloodlightProviderService.bcStore.get(cntx,
+                    IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+			
+			//Caso o pacote seja IPv4 ou IPv6
+			if(isIPv4v6(eth.getEtherType())) {
+				
+				if(isIPv4(eth.getEtherType())) {
+					IPv4 ipv4 = (IPv4) eth.getPayload();
+					ipv4.setDiffServ(IpDscp.DSCP_40.getDscpValue());
+					logger.info("IPv4 DSCP: {} ", ipv4.getDiffServ());
+					logger.info("IPv4 PROT: {} ", ipv4.getProtocol().toString());
+				} else if(isIPv4(eth.getEtherType())) {
+					IPv6 ipv6 = (IPv6) eth.getPayload();
+					logger.info("IPv6 DSCP: {} ", ipv6.getTrafficClass());
+				}
+				
+				
+				
+			}        	
 			break;
 
 		default:
 			break;
-		}
-		
-//		Ethernet eth =
-//                IFloodlightProviderService.bcStore.get(cntx,
-//                                            IFloodlightProviderService.CONTEXT_PI_PAYLOAD); 
-//        Long sourceMACHash = eth.getSourceMACAddress().getLong();
-//        if (!macAddresses.contains(sourceMACHash)) {
-//            macAddresses.add(sourceMACHash);
-//            logger.info("MAC Address: {} seen on switch: {}",
-//                    eth.getSourceMACAddress().toString(),
-//                    sw.getId().toString());
-//        }
-        
+		}        
         
         return Command.CONTINUE;
 	}
+	
+	
+	private boolean isIPv4v6(EthType eth){
+		return (EthType.IPv4 == eth) || (EthType.IPv6 == eth); 
+	}
+	
+	private boolean isIPv4(EthType eth) {
+		return EthType.IPv4 == eth;
+	}
+	
+	private boolean isIPv6(EthType eth) {
+		return EthType.IPv6 == eth;
+	}
+	
 	
 	
 	/**
@@ -400,15 +420,15 @@ public class QoS implements IOFMessageListener, IFloodlightModule, IQoSService {
         	for( Iterator<IResultSet> iter = serviceSet.iterator(); iter.hasNext();){
         		row = iter.next().getRow();
         		
-        		QoSTypeOfService s = new QoSTypeOfService();
+        		QoSTypeOfService service = new QoSTypeOfService();
         		if(!row.containsKey(COLUMN_SID) 
         				|| !row.containsKey(COLUMN_TOSBITS)){
         			logger.error("Skipping entry with required fields {}", row);
         			continue;
         		}
         		try{
-        			s.sid = Integer.parseInt((String) row.get(COLUMN_SID));
-        			s.tos = Byte.parseByte((String) row.get(COLUMN_TOSBITS));
+        			service.sid = Integer.parseInt((String) row.get(COLUMN_SID));
+        			service.tos = Byte.parseByte((String) row.get(COLUMN_TOSBITS));
         			
         			for(String key: row.keySet()){
         				if(row.get(key) == null){
@@ -419,16 +439,17 @@ public class QoS implements IOFMessageListener, IFloodlightModule, IQoSService {
         					continue;
         				}
         				else if(key.equals(COLUMN_SNAME)){
-        					s.name = (String) row.get(COLUMN_SNAME);
+        					service.name = (String) row.get(COLUMN_SNAME);
         				}
         			}
         			
         		}catch(ClassCastException e){
         			logger.error("Error, Skipping rule, Bad Data "
-        					+ e.getMessage()+" on Rule {}", s.sid);
+        					+ e.getMessage()+" on Rule {}", service.sid);
         		} 
-        		if(s.tos != -1){
-        		l.add(s);
+        		if(service.tos != -1){
+        		logger.info("readServicesFromStorage QoSTypeOfService: {}", service.toString());
+        		l.add(service);
         		}
         	}
         }catch(StorageException e){
@@ -519,19 +540,106 @@ public class QoS implements IOFMessageListener, IFloodlightModule, IQoSService {
 
 	@Override
 	public void addPolicyToNetwork(QoSPolicy policy) {
-		// TODO Auto-generated method stub
+	/*	OFFlowMod flow = policyToFlowMod(policy);
+		
+		logger.info("adding policy-flow {} to all switches",flow.toString());
+		//add to all switches
+		Map<DatapathId, IOFSwitch> switches = switchService.getAllSwitchMap();
+		//simple check
+		if(!(switches.isEmpty())){
+			for(IOFSwitch sw : switches.values()){
+				if(!(sw.isConnected())){
+					break;// cannot add
+				}
+				logger.info("Add flow Name: {} Flow: {} Switch "+ sw.getId().toString(), 
+				policy.name, flow.toString());
+				//add unique flow names based on dpid hasCode :)
+				flowPusher.addFlow(policy.name+Integer.toString(sw.getId().toString().hashCode()), 
+								  flow, sw.getId());
+				
+			}
+		}*/
 		
 	}
 
 	@Override
-	public void addPolicy(QoSPolicy policy, String swid) {
+	public synchronized void addPolicy(QoSPolicy policy, String swid) {		
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void addPolicy(QoSPolicy policy) {
-		// TODO Auto-generated method stub
+		//debug
+		logger.debug("Adding Policy to List and Storage");
+		//create the UID
+		policy.policyid = policy.genID();
+		
+		int p = 0;
+		for (p = 0; p < this.policies.size(); p++){
+			//check if empy
+			if(this.policies.isEmpty()){
+				//p is zero
+				break;
+			}
+			//starts at the first(lowest) policy based on priority
+			//insertion sort, gets hairy when n # of switches increases. 
+			//larger networks may need a merge sort.
+			if(this.policies.get(p).priority >= policy.priority){
+				//this keeps "p" in the correct position to place new policy in
+				break;
+			}
+		}
+		if (p <= this.policies.size()) {
+			this.policies.add(p, policy);
+			} 
+		else {
+			this.policies.add(policy);
+			}	
+		//Add to the storageSource
+		Map<String, Object> policyEntry = new HashMap<String, Object>();
+		policyEntry.put(COLUMN_POLID, Long.toString(policy.policyid));
+		policyEntry.put(COLUMN_NAME, policy.name);
+		policyEntry.put(COLUMN_MATCH_PROTOCOL, Short.toString(policy.protocol));
+		policyEntry.put(COLUMN_MATCH_ETHTYPE, Short.toString(policy.ethtype));
+		policyEntry.put(COLUMN_MATCH_INGRESSPRT, Short.toString(policy.ingressport));
+		policyEntry.put(COLUMN_MATCH_IPSRC, Integer.toString(policy.ipsrc));
+		policyEntry.put(COLUMN_MATCH_IPDST, Integer.toBinaryString(policy.ipdst));
+		policyEntry.put(COLUMN_MATCH_VLANID, Short.toString(policy.vlanid));
+		policyEntry.put(COLUMN_MATCH_ETHSRC, policy.ethsrc);
+		policyEntry.put(COLUMN_MATCH_ETHDST, policy.ethdst);
+		policyEntry.put(COLUMN_MATCH_TCPUDP_SRCPRT, Short.toString(policy.tcpudpsrcport));
+		policyEntry.put(COLUMN_MATCH_TCPUDP_DSTPRT, Short.toString(policy.tcpudpdstport));
+		policyEntry.put(COLUMN_NW_TOS, policy.service);
+		policyEntry.put(COLUMN_SW, policy.sw);
+		policyEntry.put(COLUMN_QUEUE, Short.toString(policy.queue));
+		policyEntry.put(COLUMN_ENQPORT, Short.toString(policy.enqueueport));
+		policyEntry.put(COLUMN_PRIORITY, Short.toString(policy.priority));
+		policyEntry.put(COLUMN_SERVICE, policy.service);
+		storageSource.insertRow(TABLE_NAME, policyEntry);
+		
+		/**
+		* TODO Morph this to use a String[] of switches
+		**/
+		
+		if (policy.sw.equals("all")){
+			logger.debug("Adding Policy {} to Entire Network", policy.toString());
+			addPolicyToNetwork(policy);
+		}
+		/** [NOTE] Note utilized yet, future revision used to "save" policies
+		 *  to the controller, then modified to be added to switched **/
+		else if (policy.sw.equals("none")){
+			logger.debug("Adding Policy {} to Controller", policy.toString());
+		}
+		//add to a specified switch b/c "all" not specified
+		else if(policy.sw.matches(dpidPattern)){
+			logger.debug("Adding policy {} to Switch {}", policy.toString(), policy.sw);
+			// add appropriate hex string converted to a long type
+			addPolicy(policy, policy.sw);
+			}	
+		else{
+			logger.error("***Policy {} error at switch input {} ***" +
+					"", policy.toString(), policy.sw);
+		}
 		
 	}
 
@@ -552,6 +660,145 @@ public class QoS implements IOFMessageListener, IFloodlightModule, IQoSService {
 		// TODO Auto-generated method stub
 		
 	}
-    
+	
+	/**
+	 * Returns a flowmod from a policy
+	 * @param policy
+	 * @return
+	 */
+	public OFFlowMod policyToFlowMod(QoSPolicy policy){
+		//initialize a match structure that matches everything
+		Match.Builder match = myFactory.buildMatch();
+		//Based on the policy match appropriately.
+	
+		//no wildcards
+		//match.setWildcards(0);
+		
+		if(policy.ethtype != -1){
+			match.setExact(MatchField.ETH_TYPE, EthType.of(policy.ethtype));
+			logger.debug("setting match on eth-type");
+		} 
 
+		if(policy.protocol != -1){
+			match.setExact(MatchField.IP_PROTO, IpProtocol.of(policy.protocol));
+			//match.setNetworkProtocol(policy.protocol);
+			//logger.debug("setting match on protocol ");
+		}
+		if(policy.ingressport != -1){
+			match.setExact(MatchField.IN_PORT,OFPort.of(policy.ingressport));
+			//match.setInputPort(policy.ingressport);
+			//logger.debug("setting match on ingress port ");
+		}
+		if(policy.ipdst != -1){
+			match.setExact(MatchField.IPV4_DST,IPv4Address.of(policy.ipdst));
+			//match.setNetworkDestination(policy.ipdst);
+			//logger.debug("setting match on network destination");
+		}
+		if(policy.ipsrc != -1){
+			match.setExact(MatchField.IPV4_SRC,IPv4Address.of(policy.ipsrc));
+			//match.setNetworkSource(policy.ipsrc);
+			//logger.debug("setting match on network source");
+		}
+		if(policy.vlanid != -1){
+			match.setExact(MatchField.VLAN_VID,OFVlanVidMatch.ofVlan(policy.vlanid));
+			//match.setDataLayerVirtualLan(policy.vlanid);
+			//logger.debug("setting match on VLAN");
+		}
+		if(policy.tos != -1){
+			match.setExact(MatchField.IP_DSCP,IpDscp.of(policy.tos));
+			//match.setNetworkTypeOfService(policy.tos);
+			//logger.debug("setting match on ToS");
+		}
+		if(policy.ethsrc != null){
+			match.setExact(MatchField.ETH_SRC,MacAddress.of(policy.ethsrc));
+			//match.setDataLayerSource(policy.ethsrc);
+			//logger.debug("setting match on data layer source");
+		}
+		if(policy.ethdst != null){
+			match.setExact(MatchField.ETH_DST,MacAddress.of(policy.ethdst));
+			//match.setDataLayerDestination(policy.ethdst);
+			//logger.debug("setting match on data layer destination");
+		}
+		if(policy.tcpudpsrcport != -1){
+			match.setExact(MatchField.TCP_SRC,TransportPort.of(policy.tcpudpsrcport));
+			//match.setTransportSource(policy.tcpudpsrcport);
+			//logger.debug("setting match on transport source port");
+		}
+		if(policy.tcpudpdstport != -1){
+			match.setExact(MatchField.TCP_DST,TransportPort.of(policy.tcpudpdstport));
+			//match.setTransportDestination(policy.tcpudpdstport);
+			//logger.debug("setting match on transport destination");
+		}
+	
+		//Create a flow mod using the previous match structure
+		OFFlowMod fm = new OFFlowMod();
+		fm.setType(OFType.FLOW_MOD);
+		//depending on the policy nw_tos or queue the flow mod
+		// will change the type of service bits or enqueue the packets
+		if(policy.queue > -1 && policy.service == null){
+			logger.info("This policy is a queuing policy");
+			List<OFAction> actions = new ArrayList<OFAction>();
+			
+			//add the queuing action
+			OFActionEnqueue enqueue = new OFActionEnqueue();
+			enqueue.setLength((short) 0xffff);
+			enqueue.setType(OFActionType.OPAQUE_ENQUEUE); // I think this happens anyway in the constructor
+			enqueue.setPort(policy.enqueueport);
+			enqueue.setQueueId(policy.queue);
+			actions.add((OFAction) enqueue);
+			
+			logger.info("Match is : {}", match.toString());
+			//add the matches and actions and return
+			fm.setMatch(match)
+				.setActions(actions)
+				.setIdleTimeout((short) 0)  // infinite
+				.setHardTimeout((short) 0)  // infinite
+				.setBufferId(OFPacketOut.BUFFER_ID_NONE)
+				.setFlags((short) 0)
+				.setOutPort(OFPort.OFPP_NONE.getValue())
+				.setPriority(policy.priority)
+				.setLengthU((short)OFFlowMod.MINIMUM_LENGTH + OFActionEnqueue.MINIMUM_LENGTH);
+				
+		}
+		else if(policy.queue == -1 && policy.service != null){
+			logger.info("This policy is a type of service policy");
+			List<OFAction> actions = new ArrayList<OFAction>();
+			
+			//add the queuing action
+			OFActionNetworkTypeOfService tosAction = new OFActionNetworkTypeOfService();
+			tosAction.setType(OFActionType.SET_NW_TOS);
+			tosAction.setLength((short) 0xffff);
+			
+			//Find the appropriate type of service bits in policy
+			Byte pTos = null;
+			List<QoSTypeOfService> serviceList = this.getServices();
+			for(QoSTypeOfService s : serviceList){
+				if(s.name.equals(policy.service)){
+					//policy's service ToS bits
+					pTos = s.tos;
+				}
+			}
+			tosAction.setNetworkTypeOfService(pTos);
+			actions.add((OFAction)tosAction);
+			
+			logger.info("Match is : {}", match.toString());
+			//add the matches and actions and return.class.ge
+			fm.setMatch(match)
+				.setActions(actions)
+				.setIdleTimeout((short) 0)  // infinite
+				.setHardTimeout((short) 0)  // infinite
+				.setBufferId(OFPacketOut.BUFFER_ID_NONE)
+				.setFlags((short) 0)
+				.setOutPort(OFPort.OFPP_NONE.getValue())
+				.setPriority(Short.MAX_VALUE)
+				.setLengthU((short)OFFlowMod.MINIMUM_LENGTH + OFActionNetworkTypeOfService.MINIMUM_LENGTH);
+		}
+		else{
+			logger.error("Policy Misconfiguration");
+		}
+	return fm;	
+	}
+	
+	
+	
 }
